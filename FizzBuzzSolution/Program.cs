05e740f982d2dce9f1f16d07859accdbe8bae7a3
Jef Larremore <jef.larremore@gmail.com>
2021-06-19 21:46:42 -0500

    Initial

---
+++ FizzBuzzSolution/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzSolution
+{
+    class Program
+    {
+        // generates a list of integers from 1 to 100
+        public static List<int> GetIntegers()
+        {
+            var integerList = new List<int>();
+            for (var i = 1; i <= 100; i++)
+            {
+                integerList.Add(i);
+            }
+            return integerList;
+        }
+        // literates through a list of integers and write the appropriate string to the console
+        public static void FizzBuzz(List<int> integerList)
+        {
+            foreach (var item in integerList)
+            {
+                if (item % 3 == 0 && item % 5 == 0)
+                {
+                    Console.WriteLine(item.ToString() + " FizzBuzz");
+                }
+                else if (item % 3 == 0)
+                {
+                    Console.WriteLine(item.ToString() + " Fizz");
+                }
+                else if (item % 5 == 0)
+                {
+                    Console.WriteLine(item.ToString() + " Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var integerList = GetIntegers();
+            FizzBuzz(integerList);
+            Console.ReadLine();
+        }
+    }
+}
